2bb6a48d86e4815c266187e4a6e264c4fcca5ace
Joey <seyoung1st@gmail.com>
2023-02-26 23:34:57 +0900

    변수란

---
+++ JavaScript/1.variable.js
@@ -0,0 +1,15 @@
+//변수란?
+//값을 저장하는공간
+//자료를 저장할 수 있는 이름이 주어진 기억장소
+
+let a = 0;
+console.log(a);
+
+a = 1; //값의 재할당
+console.log(a);
+
+let b;
+console.log(b); //undefine
+
+b = 2;
+console.log(b); //2
