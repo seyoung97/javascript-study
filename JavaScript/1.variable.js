240ba4c3ca0ae9f42371f43e35ee8c37ba2c91a7
Joey <seyoung1st@gmail.com>
2023-02-27 00:02:36 +0900

    변수 이름

--- JavaScript/1.variable.js
+++ JavaScript/1.variable.js
@@ -13,3 +13,24 @@ console.log(b); //undefine
 
 b = 2;
 console.log(b); //2
+
+// 나쁜예제: 너무 광범위해서 어떤 의미인지 유추 불가
+let number = 26;
+
+// 좋은예제
+let myAge = 26;
+// 숫자에 의미를 부여
+
+// 나쁜예제: 여러개의 변수를 1,2,3... 숫자로 구분
+let audio1;
+let audio2;
+
+// 좋은예제
+let backgroundAudio;
+let windAudio;
+
+//꿀팁
+//구체적인 걸 뒤로 뺴기
+let audioBackground;
+let audioWind;
+// 이렇게하면 나중에 audio만 쳐도 자동 완성으로 관련된 변수들이 바로 보임
