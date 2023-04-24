ccea187c11585e8ba0a2f194969c9afe6923e8de
Joey <seyoung1st@gmail.com>
2023-04-24 14:32:23 +0900

    2-4 assignment operators

---
+++ JavaScript/4.assignment.js
@@ -0,0 +1,14 @@
+//할당연산자 Assignment operators
+// 하나의 =
+let a = 1;
+a = a + 2;
+console.log(a); // 3
+
+a += 2; // a= a + 2; 의 축약 버전
+console.log(a); // 5
+
+a -= 2; // a= a-2
+a *= 2; // a = a* 2
+a /= 2;
+a %= 2;
+a **= 2;
