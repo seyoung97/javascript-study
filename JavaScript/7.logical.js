0805cef625242c81bd34626eb8f7cc425ba62ef0
Joey <seyoung1st@gmail.com>
2023-04-27 23:57:02 +0900

    3-7 logical

---
+++ JavaScript/7.logical.js
@@ -0,0 +1,17 @@
+// 논리연산자 Logical operators
+// && 그리고
+// || 또는
+// ! 부정(단항연산자에서 온것)
+// !! 불리언 값으로 변환 (단항연산자 응용버전)
+
+// && 예제
+let num = 8;
+if (num >= 0 && num < 9) {
+  console.log("굿");
+}
+
+// || 예제
+let num2 = 21;
+if (num2 >= 0 || num2 < 9) {
+  console.log("굿");
+}
