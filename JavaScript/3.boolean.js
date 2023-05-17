64593934999b2d1d9b5b0ad338e4a95374697286
정세영 <jeongseyeong@selauiMcBookAir.localdomain>
2023-05-17 17:37:05 +0900

    7-3 boolean

---
+++ JavaScript/3.boolean.js
@@ -0,0 +1,27 @@
+// Bulean Object
+true.valueOf() 
+// 불리언값 출력
+
+// 원시타입으로 바로 할당
+const isTrue = true
+
+// wrapper 객체를 통해서 할당
+const issTrue = new Boolean(true)
+
+/* 
+* Falshy
+- 0
+- -0
+- null
+- NaN
+- undefined
+- '' 빈문자열
+
+* Truthy
+- 1
+- -1
+- '0' 문자열 0
+- 'false' 문자열 false
+- [] 빈 배열
+- {} 빈 객체
+*/
